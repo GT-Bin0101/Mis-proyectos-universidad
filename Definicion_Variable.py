f4ce6ae9d4a7f998d41fe7f69150df4050d51ff4
Fabian <fabi213121@gmail.com>
2026-08-26 19:02:43 -0500

    Mis Proyectos

---
+++ Definicion_Variable.py
@@ -0,0 +1,3 @@
+#Una variable es un espacio en memoria que se utiliza para almacenar datos, los cuales pueden ser modificados durante la ejecución de un programa. En Python, las variables se crean al asignarles un valor utilizando el operador de asignación (=). Por ejemplo:
+#nombre = "Juan"
+#edad = 25
